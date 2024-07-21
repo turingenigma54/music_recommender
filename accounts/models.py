5e2906b942de964ed5e2aa96abb4a058190c45af
Erik Nurja <eriknurja@hotmail.com>
2024-07-21 14:04:45 -0700

    Add new files

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    spotify_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    spotify_access_token = models.CharField(max_length=255, null=True, blank=True)
-    spotify_refresh_token = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     
 
     def __str__(self):
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth import authenticate , login , logout
-from .forms import CustomUserCreationForm ,  CustomAuthenticationForm
-from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
 
-class RegisterView(View):
-    def get(self, request):
-        form = CustomUserCreationForm()
-        return render(request, 'register.html', {'form': form})
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {"form": form})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('home')
