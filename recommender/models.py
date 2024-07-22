cac434aeb54f0d410f538d559c8ab528162556b5
Erik Nurja <eriknurja@hotmail.com>
2024-07-22 14:09:08 -0700

    Using different database

--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+    track_id = models.CharField(max_length=255, unique=True)
+    artists = models.TextField()
+    album_name = models.TextField()
+    track_name = models.TextField()
+    popularity = models.IntegerField()
+    duration_ms = models.IntegerField()
+    explicit = models.BooleanField()
+    danceability = models.FloatField()
+    energy = models.FloatField()
+    key = models.IntegerField()
+    loudness = models.FloatField()
+    mode = models.IntegerField()
+    speechiness = models.FloatField()
+    acousticness = models.FloatField()
+    instrumentalness = models.FloatField()
+    liveness = models.FloatField()
+    valence = models.FloatField()
+    tempo = models.FloatField()
+    time_signature = models.IntegerField()
+    track_genre = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.track_name
--- spotify_recommender/settings.py
+++ spotify_recommender/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'spotify_recommender.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotify_recommender',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
