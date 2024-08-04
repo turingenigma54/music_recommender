d1f584fa07e2ea507211a25168f388ab72d88e97
Erik Nurja <eriknurja@hotmail.com>
2024-08-04 12:16:04 -0700

    Cleanup

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+
     
 
     def __str__(self):
