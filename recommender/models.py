1ac3f909385d82362f6f36951b6fe46d5ef53170
Erik Nurja <eriknurja@hotmail.com>
2024-08-04 11:52:35 -0700

    Cleanup

--- recommender/models.py
+++ recommender/models.py
@@ -1,31 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-class Track(models.Model):
-    track_id = models.CharField(max_length=255, unique=True)
-    artists = models.TextField()
-    album_name = models.TextField()
-    name = models.TextField()
-    popularity = models.IntegerField()
-    duration_ms = models.IntegerField()
-    explicit = models.BooleanField()
-    danceability = models.FloatField()
-    energy = models.FloatField()
-    key = models.IntegerField()
-    loudness = models.FloatField()
-    mode = models.IntegerField()
-    speechiness = models.FloatField()
-    acousticness = models.FloatField()
-    instrumentalness = models.FloatField()
-    liveness = models.FloatField()
-    valence = models.FloatField()
-    tempo = models.FloatField()
-    time_signature = models.IntegerField()
-    track_genre = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.name} by {self.artists}"
-
 
 class NewTrack(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
